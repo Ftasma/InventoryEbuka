534638ddfc29c9c187a6d7b93074e3f32f521d0d
BAMSSA-DEVS <bamssadevs@outlook.com>
2025-02-16 11:41:02 +0100

    Logout

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,43 @@ import { Eye, Plus, Upload, User } from "lucide-react";
 import Link from "next/link";
 import withAuth from "../withAuth"
 import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogClose, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { DialogTitle } from "@radix-ui/react-dialog";
 const Home = () => {
   const [userName, setUserName]= useState<any>()
   useEffect(()=>{
     setUserName(localStorage.getItem("userName"))
   },[])
+  const logOut = ()=>{
+    localStorage.removeItem("userName")
+    localStorage.removeItem("token")
+    location.replace("/sign-in")
+  }
   return (
     <section className="bg-[#EFEFF0] min-h-screen w-sreen">
-      <div className="flex gap-3 items-center px-6 py-4">
+      <div className="flex items-center justify-between px-5">
+        <div className="flex gap-3 items-center  px-6 py-4">
         <User size={26}/>
         <aside className="flex flex-col">
           <p className="font-bold text-[1.5rem]">Hi {userName||"User"}</p>
           <p className="font-semibold">Welcome back!</p>
         </aside>
+        </div>
+        <Dialog>
+          <DialogTrigger>
+            <Button>Logout</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogTitle>Are you sure you want to logout?</DialogTitle>
+            <DialogClose asChild>
+              <Button>Cancel</Button>
+            </DialogClose>
+            <DialogClose asChild>
+              <Button onClick={logOut}>Logout</Button>
+            </DialogClose>
+          </DialogContent>
+        </Dialog>
       </div>
       <div className="flex flex-wrap justify-center items-center h-screen gap-5">
         <Link href="/add-product">   
